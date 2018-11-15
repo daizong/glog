641758f2d7bd823470a4c13ad81ef6f64adf252d
daizong <daizong.zhang@cybex.io>
2018-11-15 19:12:38 +0800

    support set calldepth

--- example/example.go
+++ example/example.go
@@ -8,8 +8,9 @@ import (
 )
 
 func main() {
-	logger := glog.NewLogger(os.Stdout, "", log.LstdFlags, glog.DEBUG)
-
+	logger := glog.NewLogger(os.Stdout, "", log.LstdFlags|log.Lshortfile, glog.DEBUG)
+	logger.SetCallDepth(2)
+	logger.SetLevel(glog.INFO)
 	logger.Debugf("current level: %s, %s", logger.GetLevelString(), "this a debug log")
 	logger.Infof("current level: %s, %s", logger.GetLevelString(), "this a info log")
 	logger.Warningf("current level: %s, %s", logger.GetLevelString(), "this a warning log")
--- glog.go
+++ glog.go
@@ -1,39 +1,49 @@
 package glog
 
 import (
+	"fmt"
 	"io"
 	"log"
 )
 
 const (
-	DEBUG   = "DEBUG"
-	INFO    = "INFO"
+	// DEBUG level
+	DEBUG = "DEBUG"
+	// INFO level
+	INFO = "INFO"
+	// WARNING level
 	WARNING = "WARNING"
-	ERROR   = "ERROR"
-	FATAL   = "FATAL"
+	// ERROR level
+	ERROR = "ERROR"
+	// FATAL level
+	FATAL = "FATAL"
 )
 
 const (
-	INT_DEBUG   = 1
-	INT_INFO    = 2
-	INT_WARNING = 3
-	INT_ERROR   = 4
-	INT_FATAL   = 5
+	debugFlag   = 1
+	infoFlag    = 2
+	warningFlag = 3
+	errorFlag   = 4
+	fatalFlag   = 5
 )
 
 var levelMap = map[string]int{
-	DEBUG:   INT_DEBUG,
-	INFO:    INT_INFO,
-	WARNING: INT_WARNING,
-	ERROR:   INT_ERROR,
-	FATAL:   INT_FATAL,
+	DEBUG:   debugFlag,
+	INFO:    infoFlag,
+	WARNING: warningFlag,
+	ERROR:   errorFlag,
+	FATAL:   fatalFlag,
 }
 
+var calldepth = 2
+
+// Logger logger
 type Logger struct {
 	*log.Logger
 	level int
 }
 
+// NewLogger return new logger
 func NewLogger(out io.Writer, prefix string, flag int, level string) *Logger {
 	if _, ok := levelMap[level]; !ok {
 		return nil
@@ -41,6 +51,7 @@ func NewLogger(out io.Writer, prefix string, flag int, level string) *Logger {
 	return &Logger{log.New(out, prefix, flag), levelMap[level]}
 }
 
+// SetOutput set new output
 func (l *Logger) SetOutput(out io.Writer) {
 	if out == nil {
 		return
@@ -48,84 +59,99 @@ func (l *Logger) SetOutput(out io.Writer) {
 	l.Logger.SetOutput(out)
 }
 
+// SetLevel set new log level
 func (l *Logger) SetLevel(level string) {
 	if ilvl, ok := levelMap[level]; ok {
 		l.level = ilvl
 	}
 }
 
+// SetCallDepth set new calldepth
+func (l *Logger) SetCallDepth(depth int) {
+	if depth > 0 {
+		calldepth = depth
+	}
+}
+
+// GetLevel return log level int
 func (l *Logger) GetLevel() int {
 	return l.level
 }
 
+// GetLevelString get log level string
 func (l *Logger) GetLevelString() string {
 	switch l.level {
-	case INT_DEBUG:
+	case debugFlag:
 		return DEBUG
-	case INT_INFO:
+	case infoFlag:
 		return INFO
-	case INT_WARNING:
+	case warningFlag:
 		return WARNING
-	case INT_ERROR:
+	case errorFlag:
 		return ERROR
-	case INT_FATAL:
+	case fatalFlag:
 		return FATAL
 	}
 
 	return "WRONG_LEVEL"
 }
 
+// Debugf print debug log
 func (l *Logger) Debugf(format string, v ...interface{}) {
-	if l.level > INT_DEBUG {
+	if l.level > debugFlag {
 		return
 	}
 	if len(v) == 0 {
-		l.Println("[DEBUG] " + format)
+		l.Output(calldepth, fmt.Sprintln("[DEBUG] "+format))
 	} else {
-		l.Printf("[DEBUG] "+format, v...)
+		l.Output(calldepth, fmt.Sprintf("[DEBUG] "+format, v...))
 	}
 }
 
+// Infof print info log
 func (l *Logger) Infof(format string, v ...interface{}) {
-	if l.level > INT_INFO {
+	if l.level > infoFlag {
 		return
 	}
 	if len(v) == 0 {
-		l.Println("[INFO] " + format)
+		l.Output(calldepth, fmt.Sprintln("[INFO] "+format))
 	} else {
-		l.Printf("[INFO] "+format, v...)
+		l.Output(calldepth, fmt.Sprintf("[INFO] "+format, v...))
 	}
 }
 
+// Warningf print warning log
 func (l *Logger) Warningf(format string, v ...interface{}) {
-	if l.level > INT_WARNING {
+	if l.level > warningFlag {
 		return
 	}
 	if len(v) == 0 {
-		l.Println("[WARNING] " + format)
+		l.Output(calldepth, fmt.Sprintln("[WARNING] "+format))
 	} else {
-		l.Printf("[WARNING] "+format, v...)
+		l.Output(calldepth, fmt.Sprintf("[WARNING] "+format, v...))
 	}
 }
 
+// Errorf print error log
 func (l *Logger) Errorf(format string, v ...interface{}) {
-	if l.level > INT_ERROR {
+	if l.level > errorFlag {
 		return
 	}
 	if len(v) == 0 {
-		l.Println("[ERROR] " + format)
+		l.Output(calldepth, fmt.Sprintln("[ERROR] "+format))
 	} else {
-		l.Printf("[ERROR] "+format, v...)
+		l.Output(calldepth, fmt.Sprintf("[ERROR] "+format, v...))
 	}
 }
 
+// Fatalf print fatal log
 func (l *Logger) Fatalf(format string, v ...interface{}) {
-	if l.level > INT_FATAL {
+	if l.level > fatalFlag {
 		return
 	}
 	if len(v) == 0 {
-		l.Println("[FATAL] " + format)
+		l.Output(calldepth, fmt.Sprintln("[FATAL] "+format))
 	} else {
-		l.Printf("[FATAL] "+format, v...)
+		l.Output(calldepth, fmt.Sprintf("[FATAL] "+format, v...))
 	}
 }
